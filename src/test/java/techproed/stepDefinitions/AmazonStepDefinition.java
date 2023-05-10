2183d7ea5d14156b1ae78a29901e17613491610c
saidErsoz <m.said.ersoz3443@gmail.com>
2023-05-10 22:13:05 +0300

    10/05

--- src/test/java/techproed/stepDefinitions/AmazonStepDefinition.java
+++ src/test/java/techproed/stepDefinitions/AmazonStepDefinition.java
@@ -4,20 +4,44 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import techproed.pages.AmazonPage;
 import techproed.utilities.ConfigReader;
 import techproed.utilities.Driver;
 
 public class AmazonStepDefinition {
+    AmazonPage amazonPage;
+
     @Given("kullanici_amazon_sayfasina_gider")
     public void kullanici_amazon_sayfasina_gider() {
         Driver.getDriver().get(ConfigReader.getProperty("amazon_Url"));
     }
+
     @And("arama_kutusunda_iphone_aratir")
     public void arama_kutusunda_iphone_aratir() {
-        Driver.getDriver().findElement(By.id("twotabsearchtextbox")).sendKeys("iphone", Keys.ENTER);
+        amazonPage = new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys("iphone" + Keys.ENTER);
     }
+
     @And("sayfayi_kapatir")
     public void sayfayi_kapatir() {
         Driver.closeDriver();
+    }
+
+    @And("arama_kutusunda_Selenium_aratir")
+    public void arama_kutusunda_selenium_aratir() {
+        amazonPage = new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys("selenium" + Keys.ENTER);
+    }
+
+    @And("arama_kutusunda_Java_aratir")
+    public void arama_kutusunda_java_aratir() {
+        amazonPage = new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys("java" + Keys.ENTER);
+    }
+
+    @And("arama_kutusunda_sql_aratir")
+    public void arama_kutusunda_sql_aratir() {
+        amazonPage = new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys("sql" + Keys.ENTER);
     }
 }
