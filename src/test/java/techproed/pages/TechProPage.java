624cccab8c1194bb6dca967a9cb6d83c5bf827bf
saidErsoz <m.said.ersoz3443@gmail.com>
2023-05-11 00:59:16 +0300

    10/05

---
+++ src/test/java/techproed/pages/TechProPage.java
@@ -0,0 +1,22 @@
+package techproed.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import techproed.utilities.Driver;
+
+public class TechProPage {
+
+    public TechProPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    /*
+
+     */
+    @FindBy(xpath = "//*[@class='eicon-close']")
+    public WebElement reklam;
+
+    @FindBy(css = "input[type='search']")
+    public WebElement searchBox;
+}
---
+++ src/test/java/techproed/runners/Runner.java
@@ -0,0 +1,28 @@
+package techproed.runners;
+
+/*
+Runner class; testNG deki .xml file larda belirttiğimiz class'ları,packageları veya methodları nasıl
+çalıştırıyorsak, Cucumber frameworkundede Runner class'ındaki tags parametresi ile belirttiğimiz
+senaryoyu çalıştırabiliriz
+ */
+//Cucumber ile JUnit'in entegre olmasını sağlayan test çalıştırıcı notasyonudur
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+//Seneryoların nerede ve nasıl çalışacağı, hangi raporu kullanacağıyla alakalı seçenekleri ayarlarız
+@CucumberOptions(features = "",
+        glue = {"techproed/stepDefinitions"},//Bu parametre ile kodlarımızı yazdığımız stepDefinition
+        //class'ının packege'ını belirtiriz
+        tags = "@gr1 and @sgl"  )
+/*
+features ===> features'ların olduğu packega'ın yolunu ver(ContentRoot)
+glue ====> stepDefinition'ların olduğu packega'ın yolunu ver(Source Root)
+tags ====> çalıştırmak istediğin grubu yaz
+ */
+
+
+public class Runner {
+}
